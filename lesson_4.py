7feb4693d3b1f7b20be3f2a375e3ebb5c79b8bb3
Руслан <SOM-2@yandex.ru>
2025-11-13 00:47:03 +0300

    Modifying

--- lesson_4.py
+++ lesson_4.py
@@ -14,11 +14,11 @@ print(f"Периметр прямоугольника: {perimeter}")
 
 number = int(input("Введите пятизначное число: "))
 
-units = number % 10           # 5 (единицы)
-tens = (number // 10) % 10    # 7 (десятки)
-hundreds = (number // 100) % 10  # 2 (сотни)
-thousands = (number // 1000) % 10  # 6 (тысячи)
-ten_thousands = number // 10000  # 4 (десятки тысяч)
+units = number % 10
+tens = (number // 10) % 10
+hundreds = (number // 100) % 10
+thousands = (number // 1000) % 10
+ten_thousands = number // 10000
 
 result = (tens ** units) * hundreds / (ten_thousands - thousands)
 
